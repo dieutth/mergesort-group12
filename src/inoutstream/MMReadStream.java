ebd5d0d5af372091cbe0d777d9b66de9d8100ff6
QUICKLY <QUICKLY@QUICKLY-PC>
2016-12-31 16:29:02 +0100

    test MMReadStream

--- src/inoutstream/MMReadStream.java
+++ src/inoutstream/MMReadStream.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -23,6 +24,15 @@ public class MMReadStream {
 			e.printStackTrace();
 		}
 	}
+	public void setBuffer(){
+		try {
+			buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public boolean isEndOfStream(){
 		return !buffer.hasRemaining();
 	}
@@ -33,14 +43,59 @@ public class MMReadStream {
 	public MMReadStream(String fileLocation){
 		try {
 			fileChannel = new RandomAccessFile(new File(fileLocation), "r").getChannel();
-			buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+//			buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	public void close(){
+		try {
+			fileChannel.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args){
+//		MMReadStream mmrs = new MMReadStream("F://Data//intergers//test_readable//small_interger_0");
+		MMReadStream mmrs = new MMReadStream("F://Data//intergers//test//test250M//f250mil_5mil");
+		MMWriteStream mmws = new MMWriteStream("F://Data//intergers//test//test250M//out_f250mil");
+		int N = 250000000;
+		int M = 1000;
+		int loop = N/M;
+//		mmrs.setBuffer(0, M*4);
+		long startTime = System.currentTimeMillis();
+		try{
+			while (loop > 0 ){
+				mmrs.setBuffer(mmrs.fileChannel.position(), M*4);
+				mmws.fileChannel.write(mmrs.buffer);
+				loop --;
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+//		for (int i = 0; i < loop; i++){
+//			try {
+//				mmrs.setBuffer(mmrs.fileChannel.position(), M*4);
+//				mmws.fileChannel.write(mmrs.buffer);
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			
+//			if (i == loop-1)
+//				break;
+//		}
+		
+		
+		System.out.println(System.currentTimeMillis() - startTime);
+		try {
+			mmrs.fileChannel.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
